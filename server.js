550485a9f16f029439a0fb5c27b170ce64754357
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2017-07-11 20:55:10 -0400

    third

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-// var app = require('./express');
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+var app = require('./express');
+var express = app.express;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.static())
 //
 // app.set('view engine', 'ejs');
 // require('./utilities/filelist');
 //
-// app.use(app.express.static(__dirname + '/public'));
+ app.use(app.express.static(__dirname + '/public'));
 //
-// require('./test/app');
+require('./test/app');
 //
-// app.listen(process.env.PORT || 3000);
-console.log("hello world");
-var express = require('express');
-var app = express();
+app.listen(process.env.PORT || 3000);
+// console.log("hello world");
+// var express = require('express');
+// var app = express();
 // app.get('/hello',function (req,res) {
 //     //res.send("hello from server.");
 //     //console.log("hello from server.");
 //     res.send({message: 'hello from server'})
 // });
 
-app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+//var port = app.use(express.static(__dirname + '/public'));
+//app.listen(3000 || port);
+
